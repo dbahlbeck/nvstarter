df98fc3e70604b7d44ba8b50c7101217b81ca912
David Ahlbeck <david.ahlbeck@scania.com>
2025-07-28 21:37:28 +0200

    Daves modifications

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,23 @@
 require "nvchad.options"
 
+require("nvim-tree").setup({
+  view = {
+    width = 50
+  },
+  renderer = {
+    icons = {
+      glyphs = {
+        git = {
+          -- unstaged = "U"
+        }
+      }
+    }
+  }
+})
+
 -- add yours here!
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+
